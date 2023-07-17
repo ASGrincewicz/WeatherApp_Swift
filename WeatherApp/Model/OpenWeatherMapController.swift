4b0e327ceb742e023af2dbbc61c3bddcb652f535
Aaron Grincewicz <asgrincewicz@icloud.com>
2023-07-17 10:48:02 -0400

    OpenWeatherMap API integration.

---
+++ WeatherApp/Model/OpenWeatherMapController.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherMapController.swift
+//  WeatherApp
+//
+//  Created by Aaron Grincewicz on 7/17/23.
+//
+
+import Foundation
+
+class OpenWeatherMapController: WebServiceController{
+	func fetchWeatherData(for city: String, completionHandler: (String?, WebServiceControllerError?) -> Void) {
+		
+	}
+	
+	
+	
+}
